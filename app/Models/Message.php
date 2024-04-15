e2f68a7a20998d295cd14cfaec67b620b752a64d
Brian Barros <brian.ferreira@pie.tax>
2024-04-15 12:29:37 -0300

    Creating a business model for messaging

---
+++ app/Models/Message.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\{
+    MorphTo,
+    BelongsTo,
+};
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'text'
+    ];
+
+    /**
+     * Get the topic that owns the message.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function topic(): BelongsTo
+    {
+        return $this->belongsTo(Topic::class);
+    }
+
+    /**
+     * Get the parent messageable model (user or admin).
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function messageable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
