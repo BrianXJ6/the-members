20cad7909f67941e91fbaeb674fedfaebe703260
Brian Barros <brian.ferreira@pie.tax>
2024-04-14 21:00:07 -0300

    Update legacy migrations

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -1,13 +1,15 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up(): void
     {
@@ -47,6 +49,8 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
     public function down(): void
     {
